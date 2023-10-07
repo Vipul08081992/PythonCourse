be25bac0f6db09e4e1d249c38012d8abb67e79f2
Vipul08081992 <147226659+Vipul08081992@users.noreply.github.com>
2023-10-07 19:34:53 +0530

    Add files via upload

---
+++ Laboo1.py
@@ -0,0 +1,12 @@
+
+#Task1
+print(input("Enter your name here: "))
+
+#Task2
+f=input("Enter your first name: ")
+l=input("Enter your Last name: ")
+print(f,l,sep='-',end='/t')
+
+#Task3
+name=input("Enter your name here: ")
+print("You are welcome <",name,"> to the Python Class.")
